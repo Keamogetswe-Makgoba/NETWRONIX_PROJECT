c6a2013e5a1814d2319d71e73b6d095ab1cb3361
Keamogetswe-Makgoba <keamogetswepulane15@gmail.com>
2026-03-20 17:35:32 +0200

    Fix: Switched to Port 465 SSL to resolve [Errno 101] Network is unreachable

--- netwronix_core/settings.py
+++ netwronix_core/settings.py
@@ -123,8 +123,9 @@ AUTH_USER_MODEL = 'portal.User'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
+EMAIL_USE_TLS = False
 EMAIL_HOST_USER = 'vaaltein.t@gmail.com' 
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD', 'siugqqccbajeizrm') 
 
